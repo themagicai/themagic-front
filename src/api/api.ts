2c5583579a1fc540c0bfb645e399f9af0907144b
ramildeveloper <ramilprogrammer@gmail.com>
2023-02-23 17:15:25 +0500

    api config

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,1 @@
 // export const API = process.env.REACT_APP_API;
-
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "http://217.76.61.33/api/",
-    headers: {
-        "Content-type": "application/json",
-    },
-});
--- src/redux/thunks/userThunk.ts
+++ src/redux/thunks/userThunk.ts
@@ -1,73 +1,73 @@
-import axios from "axios";
-import { AppDispatch } from "..";
-import { API } from "../../api";
-import { userActions } from "../slices/userSlice";
+// import axios from "axios";
+// import { AppDispatch } from "..";
+// import { API } from "../../api/api";
+// import { userActions } from "../slices/userSlice";
 
-axios.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    (error) => {
-        if (error.response.status === 401) {
-            window.localStorage.removeItem("user");
-        }
-        if (error.response.status === 403) {
-            refreshToken();
-            console.log(`403 error`);
-        }
-    }
-);
+// axios.interceptors.response.use(
+//     (response) => {
+//         return response;
+//     },
+//     (error) => {
+//         if (error.response.status === 401) {
+//             window.localStorage.removeItem("user");
+//         }
+//         if (error.response.status === 403) {
+//             refreshToken();
+//             console.log(`403 error`);
+//         }
+//     }
+// );
 
-const refreshToken = () => {
-    const refresh = restoreAuthToken();
-    axios
-        .post(`${API}/auth/refresh`, {
-            refresh,
-        })
-        .then((data) => console.log(data))
-        .catch((e) => console.log(e));
-};
+// const refreshToken = () => {
+//     const refresh = restoreAuthToken();
+//     axios
+//         .post(`${API}/auth/refresh`, {
+//             refresh,
+//         })
+//         .then((data) => console.log(data))
+//         .catch((e) => console.log(e));
+// };
 
-const setHeaderToken = (accessToken: string) =>
-    axios.interceptors.request.use(
-        (config: any) => {
-            config.headers["Authorization"] = `Bearer ${accessToken}`;
-            return config;
-        },
-        (error) => {
-            return Promise.reject(error);
-        }
-    );
+// const setHeaderToken = (accessToken: string) =>
+//     axios.interceptors.request.use(
+//         (config: any) => {
+//             config.headers["Authorization"] = `Bearer ${accessToken}`;
+//             return config;
+//         },
+//         (error) => {
+//             return Promise.reject(error);
+//         }
+//     );
 
-const restoreAuthToken = () => {
-    const authToken = window.localStorage.getItem("user");
-    if (!authToken) {
-        return null;
-    }
-    try {
-        return JSON.parse(authToken);
-    } catch (error) {
-        return null;
-    }
-};
+// const restoreAuthToken = () => {
+//     const authToken = window.localStorage.getItem("user");
+//     if (!authToken) {
+//         return null;
+//     }
+//     try {
+//         return JSON.parse(authToken);
+//     } catch (error) {
+//         return null;
+//     }
+// };
 
-export const restoreAuth = () => async (dispatch: AppDispatch) => {
-    const authToken = restoreAuthToken();
+// export const restoreAuth = () => async (dispatch: AppDispatch) => {
+//     const authToken = restoreAuthToken();
 
-    if (authToken) {
-        dispatch(userActions.loginSuccess(authToken));
-        setHeaderToken(authToken.access);
-    }
-};
+//     if (authToken) {
+//         dispatch(userActions.loginSuccess(authToken));
+//         setHeaderToken(authToken.access);
+//     }
+// };
 
-export const createAccount =
-    (userData: any) => async (dispatch: AppDispatch) => {
-        dispatch(userActions.signUpRequest());
-        try {
-            const response = await axios.post(`${API}/auth/sign_up`, userData);
-            console.log(response);
-            dispatch(userActions.signUpSuccess());
-        } catch (error) {
-            dispatch(userActions.signUpFailed());
-        }
-    };
+// export const createAccount =
+//     (userData: any) => async (dispatch: AppDispatch) => {
+//         dispatch(userActions.signUpRequest());
+//         try {
+//             const response = await axios.post(`${API}/auth/sign_up`, userData);
+//             console.log(response);
+//             dispatch(userActions.signUpSuccess());
+//         } catch (error) {
+//             dispatch(userActions.signUpFailed());
+//         }
+//     };
