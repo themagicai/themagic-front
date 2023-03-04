4a6ba361e9cfa31e31e13c0a5bf46db26a4399f6
ramildeveloper <ramilprogrammer@gmail.com>
2023-03-04 11:05:08 +0500

    test

--- src/redux/auth/auth.api.ts
+++ src/redux/auth/auth.api.ts
@@ -1,5 +1,6 @@
 import { api } from '../index.api';
 import { RegisterTypes, LoginTypes, LogoutTypes } from './auth.types';
+import { transformResponse, transformErrorResponse } from '../../utils/transformResponse';
 
 export const authApi = api.injectEndpoints({
     endpoints: (builder) => ({
@@ -10,6 +11,8 @@ export const authApi = api.injectEndpoints({
                 body,
             }),
             invalidatesTags: ['auth'],
+            transformResponse,
+            transformErrorResponse,
         }),
         loginUsers: builder.mutation<string, LoginTypes>({
             query: (body) => ({
@@ -18,6 +21,8 @@ export const authApi = api.injectEndpoints({
                 body,
             }),
             invalidatesTags: ['auth'],
+            transformResponse,
+            transformErrorResponse
         }),
         logoutUsers: builder.mutation<string, LogoutTypes>({
             query: (body) => ({
@@ -26,6 +31,7 @@ export const authApi = api.injectEndpoints({
                 body,
             }),
             invalidatesTags: ['auth'],
+            transformResponse,
         }),
     }),
 });
