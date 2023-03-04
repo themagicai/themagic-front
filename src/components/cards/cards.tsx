15f52f94a5e3975528a8762e79c2b0b17688c76d
ramildeveloper <ramilprogrammer@gmail.com>
2023-03-04 13:29:28 +0500

    test

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -7,35 +7,7 @@ import {
     CardContent,
 } from '@mui/material';
 import styles from './styles.module.scss';
-
-interface cardDataTypes {
-    id: number;
-    title: string;
-    desc: string;
-}
-
-const cardData: Array<cardDataTypes> = [
-    {
-        id: 1,
-        title: 'Selling CV',
-        desc: 'Help to package your experience in a CV for your dream Job',
-    },
-    {
-        id: 2,
-        title: 'Attractive Cover Letter',
-        desc: 'Writ e a best letter for the vacancy',
-    },
-    {
-        id: 3,
-        title: 'LinkedIn profile',
-        desc: 'Boost your LinkedIn presence and increase job invitations',
-    },
-    {
-        id: 4,
-        title: 'Prepare for an interview',
-        desc: 'Provide questions, act as an interviewer and correct errors',
-    },
-];
+import { cardData } from '../../constants/db/cards';
 
 export const Cards = () => {
     return (
---
+++ src/constants/db/cards.ts
@@ -0,0 +1,28 @@
+type cardDataTypes = {
+    id: number;
+    title: string;
+    desc: string;
+}
+
+export const cardData: Array<cardDataTypes> = [
+    {
+        id: 1,
+        title: 'Selling CV',
+        desc: 'Help to package your experience in a CV for your dream Job',
+    },
+    {
+        id: 2,
+        title: 'Attractive Cover Letter',
+        desc: 'Writ e a best letter for the vacancy',
+    },
+    {
+        id: 3,
+        title: 'LinkedIn profile',
+        desc: 'Boost your LinkedIn presence and increase job invitations',
+    },
+    {
+        id: 4,
+        title: 'Prepare for an interview',
+        desc: 'Provide questions, act as an interviewer and correct errors',
+    },
+];
