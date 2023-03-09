879daed8e8cfa05c33d86b77e652207337af1936
ramildeveloper <ramilprogrammer@gmail.com>
2023-03-09 19:41:23 +0500

    test

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -86,7 +86,7 @@ export const Register = () => {
                     className={styles.TextField}
                     {...register('email', {
                         pattern:
-                            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                        /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
                         required: true,
                         minLength: 3,
                         maxLength: 25,
